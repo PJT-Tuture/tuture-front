8949e58f2caa1074c0ab0c6c189be69a53015ed3
박수영 <qkrtudwkd@gmail.com>
2024-05-24 01:51:16 +0900

    feature: 필터기능조회 구현

--- tuture-front/src/views/BoardDetail.vue
+++ tuture-front/src/views/BoardDetail.vue
@@ -2,7 +2,11 @@
     <div class="board-detail">
         <h1>{{ board.title }}</h1>
         <p>{{ board.content }}</p>
-        <!-- 필요한 다른 정보들 표시 -->
+        
+        <div v-if="board.writer">
+            <button @click="editBoard">수정</button>
+            <button @click="deleteBoard">삭제</button>
+        </div>
     </div>
 </template>
 
@@ -32,9 +36,16 @@ export default {
                     },
                 });
                 this.board = response.data;
+                console.log(response.data);
             } catch (error) {
                 console.error('Error fetching board detail:', error);
             }
+        },
+        editBoard() {
+            this.$router.push({ name: 'EditBoard', params: { id: this.id } });
+        },
+        deleteBoard() {
+            this.$router.push({ name: 'DeleteBoard', params: { id: this.id } });
         },
     },
 };
